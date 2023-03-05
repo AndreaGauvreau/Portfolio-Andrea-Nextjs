297a4a4f53ffe2270ace13d92f83ac05c12b24d8
Andrea.Gauvreau <andrea@teachizi.fr>
2023-03-05 01:35:19 +0100

    next image

--- src/app/components/Home/Main/animationDd.jsx
+++ src/app/components/Home/Main/animationDd.jsx
@@ -1,4 +1,5 @@
-import {Box, Image} from '@chakra-ui/react'
+import {Box} from '@chakra-ui/react'
+import Image from 'next/image'
 import React from 'react'
 
 export default function AnimationDd() {
@@ -9,16 +10,22 @@ export default function AnimationDd() {
           className="index1 animationimg"
           src="/images/identite/index-1-dd-animation.png"
           alt="banderole-qui-tourne-andrea-gauvreau"
+          width={300}
+          height={300}
         />
         <Image
           className="index2 animationimg"
           src="/images/identite/index-2-dd-animation.png"
           alt="banderole-qui-tourne-andrea-gauvreau-numero-2"
+          width={300}
+          height={300}
         />
         <Image
           className="index3 animationimg"
           src="/images/identite/index-3-dd-animation.png"
           alt="andrea-gauvreau-light"
+          width={300}
+          height={300}
         />
       </Box>
     </Box>
