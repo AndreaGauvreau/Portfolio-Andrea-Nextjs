4ce32f41b390af09c8f85173c8e2ded2c2859a6f
Andrea.Gauvreau <andrea@teachizi.fr>
2023-03-16 11:18:36 +0100

    updateshema

--- src/app/tools/glassmorphism/head.tsx
+++ src/app/tools/glassmorphism/head.tsx
@@ -10,6 +10,66 @@ export default function Head() {
         rel="canonical"
         href="https://www.andrea-gauvreau.fr/tools/glassmorphism"
       />
+
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: `{
+          '@context': 'https://schema.org',
+          '@type': 'FAQPage',
+          mainEntity: [
+            {
+              '@type': 'Question',
+              name: "Qu'est-ce que le Glassmorphism exactement ?",
+              acceptedAnswer: {
+                '@type': 'Answer',
+                text: "Le Glassmorphism est une tendance de design qui met l'accent sur l'utilisation d'effets de verre dépoli (frosted glass), avec une combinaison de transparence, d'ombres floues et de bords arrondis pour créer un style moderne et sophistiqué.",
+              },
+            },
+            {
+              '@type': 'Question',
+              name: 'Pourquoi le Glassmorphism est-il populaire ?',
+              acceptedAnswer: {
+                '@type': 'Answer',
+                text: "Le Glassmorphism est populaire parce qu'il offre un look élégant et innovant, tout en étant suffisamment polyvalent pour s'adapter à de nombreux contextes de design. Il permet de créer des interfaces et des éléments visuels attrayants et mémorables.",
+              },
+            },
+            {
+              '@type': 'Question',
+              name: 'Comment fonctionne le générateur de CSS Glassmorphism ?',
+              acceptedAnswer: {
+                '@type': 'Answer',
+                text: 'le générateur de CSS Glassmorphism te permet de personnaliser les éléments clés du style, tels que la transparence, les ombres et les bords arrondis, et de générer automatiquement le code CSS correspondant. Il suffit de sélectionner les options souhaitées et de copier-coller le code dans ton projet.',
+              },
+            },
+            {
+              '@type': 'Question',
+              name: 'Est-ce que le Glassmorphism fonctionne sur tous les navigateurs ?',
+              acceptedAnswer: {
+                '@type': 'Answer',
+                text: "Le Glassmorphism fonctionne sur la plupart des navigateurs modernes, mais il peut y avoir des problèmes de compatibilité avec certains navigateurs plus anciens. Dans ces cas-là, il est recommandé d'utiliser des préfixes spécifiques aux navigateurs ou des solutions de repli pour assurer une expérience utilisateur cohérente.",
+              },
+            },
+            {
+              '@type': 'Question',
+              name: 'Comment puis-je adapter le Glassmorphism à mon propre projet ?',
+              acceptedAnswer: {
+                '@type': 'Answer',
+                text: "Il te suffit d'utiliser le générateur pour créer le code CSS correspondant à l'effet Glassmorphism souhaité, puis d'intégrer ce code dans ton projet. Tu peux également personnaliser les couleurs, les dimensions et d'autres aspects du design pour l'adapter à tes besoins.",
+              },
+            },
+            {
+              '@type': 'Question',
+              name: 'Le générateur de CSS Glassmorphism est-il gratuit ?',
+              acceptedAnswer: {
+                '@type': 'Answer',
+                text: "Oui, le générateur est totalement gratuit et accessible à tous. Tu peux l'utiliser autant de fois que tu le souhaites pour créer des designs Glassmorphism pour tes projets personnels ou professionnels.",
+              },
+            },
+          ],
+       `,
+        }}
+      />
     </>
   )
 }
