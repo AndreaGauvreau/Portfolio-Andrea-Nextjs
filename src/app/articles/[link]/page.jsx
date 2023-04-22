ddfd6f2f81608a001fce2f4be52046f4037a59ac
Andrea.Gauvreau <andrea@teachizi.fr>
2023-04-23 01:30:58 +0200

    static metadata

--- src/app/articles/[link]/page.jsx
+++ src/app/articles/[link]/page.jsx
@@ -1,7 +1,8 @@
 import ArticlePage from '@/components/article/ArticlePage.jsx'
 import MarkdownArticle from '@/components/article/MarkdownArticle'
+import {fetchPostByLink} from '@/commons/api/post.jsx'
 
-export default function Page({params}) {
+export default async function Page({params}) {
   const post = {imagePath: '/2k_earth_nightmap.jpg'}
   return (
     <div>
@@ -9,4 +10,26 @@ export default function Page({params}) {
       <MarkdownArticle params={params} />
     </div>
   )
+}
+export async function generateMetadata({params}) {
+  const post = await fetchPostByLink(params?.link)
+  return {
+    title: post?.metaTitle,
+    description: post?.metaDescription,
+    keywords: post?.tags.map(e => e),
+    publisher: post?.auteur,
+    creator: post?.auteur,
+    authors: [
+      {name: 'Gauvreau'},
+      {name: 'andréa', url: 'https://andrea-gauvreau.fr'},
+    ],
+    metadataBase: new URL('https://www.andrea-gauvreau.fr/'),
+    alternates: {
+      canonical: `/${post?.link}`,
+    },
+    openGraph: {
+      images: post?.imagePath,
+      url: `https://www.andrea-gauvreau.fr/${post?.link}`,
+    },
+  }
 }
